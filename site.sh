98152d1ea65df5fb9505c8e4ffbf2fd794c36931
Nicholas Dille <nicholas.dille@mailbox.org>
2018-12-17 13:51:19 +0100

    It is working!!!

---
+++ site.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+ansible-playbook hcloud.yml
+
+# needs updating for hardening
+#ansible-playbook prepare.yml
+
+# needs to be run if workers are added
+ansible-playbook ca.yml
+ansible-playbook kubelets.yml
+
+ansible-playbook kube-proxy.yml
+ansible-playbook kube-controller-manager.yml
+ansible-playbook kube-scheduler.yml
+ansible-playbook kube-admin-user.yml
+ansible-playbook kubeencryptionconfig.yml
+
+ansible-playbook controller.yml
+ansible-playbook worker.yml
